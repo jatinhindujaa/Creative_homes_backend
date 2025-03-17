f30d7cca8eb6b2a0fa120cdd95f86e22db125a81
NikhilSharma-007 <niks21902@gmail.com>
2025-03-17 17:27:33 +0530

    Project setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import app from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 9000, (req, res) => {
+      console.log("App is running on port: ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed!!! ", err);
+  });
