0df5fc7d15d29e123700ef3561cbbe4a210abaab
NikhilSharma-007 <niks21902@gmail.com>
2025-03-22 18:25:00 +0530

    Add create, get all and delete contact api for contact

---
+++ src/models/contact.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Contact = mongoose.model("Contact", contactSchema);
---
+++ src/routes/contact.route.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+import {
+  createContact,
+  getAllContacts,
+  deleteContact,
+} from "../controllers/contact.controller.js";
+
+const router = Router();
+
+router.route("/create").post(createContact);
+router.route("/get-all").get(
+  // verifyJwt,
+  getAllContacts
+);
+router.route("/delete").get(
+  // verifyJwt,
+  deleteContact
+);
+
+export default router;
